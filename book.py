067580e4cf6c3756eaa47d1c7e3c88a2feabf7b3
alicia-granados <aliciagranados35@gmail.com>
2022-04-22 00:19:19 -0500

    style: :fire: id + url API

--- book.py
+++ book.py
@@ -3,8 +3,10 @@ import json
 import urllib.request
  
 # It connects to the Google API and opens it as a file, then what is brought from the API converts it into a JSON
- 
-with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=search+terms&AIzaSyDqHYhoLtp0dR28gILA4OJFWoxYxJiDu6Q") as url:
+#id = "gqDf__ULmR8C"
+id="UpNHAQAAMAAJ"
+url="https://www.googleapis.com/books/v1/volumes/"
+with urllib.request.urlopen(url+id) as url:
  
     data = json.loads(url.read().decode()) # Convert the JSON to string
     # print(data) Print data
@@ -12,42 +14,42 @@ with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=searc
 # After the variable dictlibros, add the key "items" of the json data
 dictlibros = {}
 
-dictlibros = data['items']
+dictlibros = data
  
 dictLibrosDos = {}
  
 dictLibrosTres = {}
 
 # Iterate the dictionary dictlibros to add more values ​​to the new dictionary dictLibrosDos
-for i in range(dictlibros.__len__() - 1):
-     dictLibrosDos[i] = dictlibros[i]
+
+dictLibrosDos = dictlibros
 
 # Add all books to dicLibrosDos
-for l in range(dictLibrosDos.__len__()):
+
     
     # Extract the required fields from the API
-    title = dictLibrosDos[l]["volumeInfo"].get("title", "")
-    subtitle = dictLibrosDos[l]["volumeInfo"].get("subtitle", "")
-    authors = dictLibrosDos[l]["volumeInfo"].get("authors", "")
-    publisher = dictLibrosDos[l]["volumeInfo"].get("publisher", "")
-    publishedDate = dictLibrosDos[l]["volumeInfo"].get("publishedDate", "")
-    description = dictLibrosDos[l]["volumeInfo"].get("description", "")
-    pageCount = dictLibrosDos[l]["volumeInfo"].get("pageCount", "")
-    categories = dictLibrosDos[l]["volumeInfo"].get("categories", "")
+title = dictLibrosDos["volumeInfo"].get("title", "")
+subtitle = dictLibrosDos["volumeInfo"].get("subtitle", "")
+authors = dictLibrosDos["volumeInfo"].get("authors", "")
+publisher = dictLibrosDos["volumeInfo"].get("publisher", "")
+publishedDate = dictLibrosDos["volumeInfo"].get("publishedDate", "")
+description = dictLibrosDos["volumeInfo"].get("description", "")
+pageCount = dictLibrosDos["volumeInfo"].get("pageCount", "")
+categories = dictLibrosDos["volumeInfo"].get("categories", "")
    
     # The extracted data is saved to the new dictionary
-    dictLibrosTres[l] = {
-        "title": title,
-        "subtitle":subtitle,
-        "authors": authors,
-        "publisher":publisher,
-        "publishedDate":publishedDate,
-        "description": description,
-        "pageCount": pageCount,
-        "categories": categories
-    }
+dictLibrosTres = {
+    "title": title,
+    "subtitle":subtitle,
+    "authors": authors,
+    "publisher":publisher,
+    "publishedDate":publishedDate,
+    "description": description,
+    "pageCount": pageCount,
+    "categories": categories
+}
     
   
-for j in range(dictLibrosTres.__len__() - 1):  # Go through the dictionary of dicLibrosTres
+  # Go through the dictionary of dicLibrosTres
 
-    print(dictLibrosTres[j]) # Insert books to the collection
+print(dictLibrosTres) # Insert books to the collection
