210e2889bf03bcc5b9a5b3cfc679a0651194d25c
AlfonsoLoopez <92704004+AlfonsoLoopez@users.noreply.github.com>
2022-05-15 19:05:56 -0500

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements2.txt
 
 COPY . .
 
-CMD [ "python", "Interfaz.py" ]
+CMD [ "python", "book.py" ]
--- book.py
+++ book.py
@@ -37,3 +37,8 @@ class Book:
             return self.dictLibrosTres
         except:
             return "No Existe"
+
+if __name__ == "__main__":
+    libro = Book()
+    a = libro.ExtraerElementos("UpNHAQAAMAAJ")
+    print(a)
